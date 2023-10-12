904c6c375496c6c8c666b21dd0da0ea12246760e
amennajam009 <amennajam7@gmail.com>
2023-10-12 23:12:36 +0500

    add

--- src/app/angular-bootstrap/angular-bootstrap.module.ts
+++ src/app/angular-bootstrap/angular-bootstrap.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { AngularBootstrapRoutingModule } from './angular-bootstrap-routing.module';
 import { AngularBootstrapComponent } from './angular-bootstrap.component';
+import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
   declarations: [
-    AngularBootstrapComponent
+    AngularBootstrapComponent,
+    HomeComponent
   ],
   imports: [
     CommonModule,
--- src/app/angular-material/angular-material.module.ts
+++ src/app/angular-material/angular-material.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { AngularMaterialRoutingModule } from './angular-material-routing.module';
 import { AngularMaterialComponent } from './angular-material.component';
+import { HomeComponent } from './home/home.component';
 
 
 
 @NgModule({
   declarations: [
     AngularMaterialComponent,
+    HomeComponent,
   ],
   imports: [
     CommonModule,
