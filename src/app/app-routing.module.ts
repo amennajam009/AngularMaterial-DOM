b44035f6192848a6b9d6d9ecb4b64bef0e94e1dc
amennajam009 <amennajam7@gmail.com>
2023-10-12 21:35:48 +0500

    add

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'AngularMaterial', loadChildren: () => import('./angular-material/angular-material.module').then(m => m.AngularMaterialModule) }, { path: 'Angular-bootstrap', loadChildren: () => import('./angular-bootstrap/angular-bootstrap.module').then(m => m.AngularBootstrapModule) }];
+const routes: Routes = [
+  { path: 'AngularMaterial', loadChildren: () => import('./angular-material/angular-material.module').then(m => m.AngularMaterialModule) }, 
+  { path: 'Angular-bootstrap', loadChildren: () => import('./angular-bootstrap/angular-bootstrap.module').then(m => m.AngularBootstrapModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
