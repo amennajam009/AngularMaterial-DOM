363a4676dae206d3ae0d7995ca8d491e2774c66b
amennajam009 <amennajam7@gmail.com>
2023-10-13 21:21:26 +0500

    add

--- src/app/angular-material/angular-material-routing.module.ts
+++ src/app/angular-material/angular-material-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AngularMaterialComponent } from './angular-material.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [{ path: '', component: AngularMaterialComponent }];
+const routes: Routes = [
+  { path: '', component: AngularMaterialComponent,
+  children:[
+    {path:'' , component:HomeComponent},
+
+  
+  
+  ] }
+  
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
