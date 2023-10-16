26b4bf4e2fdd4c4f434e81c7fb4ac785f2b9d418
amennajam009 <amennajam7@gmail.com>
2023-10-17 02:47:17 +0500

    add

--- src/app/angular-bootstrap/shared/bootstrap-service.service.ts
+++ src/app/angular-bootstrap/shared/bootstrap-service.service.ts
@@ -7,7 +7,15 @@ export class BootstrapServiceService {
 
   constructor(private HttpClient:HttpClient) {}
 
+
+//Getting the Variables
   GetDropDownVariables(){
     return this.HttpClient.get("http://localhost:8080/SendVariableData/SendDropDownVariables")
+  }
+
+
+// Saving the Variable
+  saveVariables(payLoad:any){
+    return this.HttpClient.post("http://localhost:8080/SendVariableData/SendDropDownVariables" , payLoad)
   }
 }
