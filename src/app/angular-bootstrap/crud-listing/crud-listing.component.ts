26b1baab9c5968b504727e2f22aeafa6baa9ed4c
amennajam009 <amennajam7@gmail.com>
2023-11-07 23:39:41 +0500

    add

--- src/app/angular-bootstrap/crud-listing/crud-listing.component.ts
+++ src/app/angular-bootstrap/crud-listing/crud-listing.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { BootstrapServiceService } from '../shared/bootstrap-service.service';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-crud-listing',
@@ -8,8 +9,12 @@ import { BootstrapServiceService } from '../shared/bootstrap-service.service';
 })
 export class CrudListingComponent {
  UserDatalist:any = []
-
-  constructor(private bootStrapService:BootstrapServiceService) { }
+ updatedvalues:any = []
+ UserUpdateFrom: FormBuilder | any
+  constructor(private bootStrapService:BootstrapServiceService,
+              private FormBuilder:FormBuilder) { 
+    this.userUpdateFromModel()
+  }
   
   ngOnInit(): void {
     this.getUserData()
@@ -21,7 +26,6 @@ export class CrudListingComponent {
 getUserData(){
   this.bootStrapService.getUserData().subscribe((res:any)=>{
     this.UserDatalist = res
-    
   })
 }
 
@@ -38,4 +42,22 @@ deleteUserDataById(id:any){
     this.UserDatalist = this.UserDatalist?.filter((list: any) => list.id !== id);
   })
 }
+
+userUpdateFromModel(){
+  this.UserUpdateFrom = this.FormBuilder.group({
+    name: new FormControl (this.UserDatalist?.name,[Validators.required]),
+    email: new FormControl (this.UserDatalist?.email,[Validators.required]),
+    first_name: new FormControl (this.UserDatalist?.first_name,[Validators.required,]),
+    last_name: new FormControl (this.UserDatalist?.last_name,[Validators.required,]),
+    address: new FormControl ('',[Validators.required,]),
+    city: new FormControl ('',[Validators.required,]),
+    country: new FormControl ('',[Validators.required,]),
+    pin_code: new FormControl ('',[Validators.required,]),
+    about_me: new FormControl ('',[Validators.required,]),
+    select1: new FormControl ('',[Validators.required,]), 
+    select2: new FormControl ('',[Validators.required,])  
+  })
+}
+
+
 }
