7c05183f48724de83fee5898ddd42a35c55c000f
amennajam009 <amennajam7@gmail.com>
2023-11-07 20:47:49 +0500

    add

--- src/app/angular-bootstrap/crud-main/crud-main.component.ts
+++ src/app/angular-bootstrap/crud-main/crud-main.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-crud-main',
@@ -6,5 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./crud-main.component.css']
 })
 export class CRUDMainComponent {
+
+  UserFrom: FormBuilder | any
+
+
+constructor(private FormBuilder:FormBuilder) { }
+
+ngOnInit(): void {
+
+}
+
+userFromModel(){
+  this.UserFrom = this.FormBuilder.group({
+    variable1: new FormControl ('',[Validators.required]),
+    variable2: new FormControl ('',[Validators.required]),
+    variable3: new FormControl ('',[Validators.required,]),
+    variable4: new FormControl ('',[Validators.required,]),
+    variable5: new FormControl ('',[Validators.required,]),
+    variable6: new FormControl ('',[Validators.required,]),
+    variable7: new FormControl ('',[Validators.required,]),
+    variable8: new FormControl ('',[Validators.required,]),
+    variable9: new FormControl ('',[Validators.required,]),
+  })
+}
 
 }
