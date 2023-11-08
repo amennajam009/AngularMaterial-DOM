db438b1a065c2ebf24f16bb85948c1b0046a7ab4
amennajam009 <amennajam7@gmail.com>
2023-11-08 12:10:29 +0500

    add

--- src/app/angular-bootstrap/crud-listing/crud-listing.component.ts
+++ src/app/angular-bootstrap/crud-listing/crud-listing.component.ts
@@ -67,11 +67,11 @@ getUpdateFromModel(id:any){
     email: new FormControl (this.DataById?.email,[Validators.required]),
     first_name: new FormControl (this.DataById?.first_name,[Validators.required,]),
     last_name: new FormControl (this.DataById?.last_name,[Validators.required,]),
-    address: new FormControl ('',[Validators.required,]),
-    city: new FormControl ('',[Validators.required,]),
-    country: new FormControl ('',[Validators.required,]),
-    pin_code: new FormControl ('',[Validators.required,]),
-    about_me: new FormControl ('',[Validators.required,]),
+    address: new FormControl (this.DataById?.address,[Validators.required,]),
+    city: new FormControl (this.DataById?.city,[Validators.required,]),
+    country: new FormControl (this.DataById?.country,[Validators.required,]),
+    pin_code: new FormControl (this.DataById?.pin_code,[Validators.required,]),
+    about_me: new FormControl (this.DataById?.about_me,[Validators.required,]),
     select1: new FormControl ('',[Validators.required,]), 
     select2: new FormControl ('',[Validators.required,])  
   })
