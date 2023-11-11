4bffe01a6b17aa1a1aa974a4f68b63aed58c2350
amennajam009 <amennajam7@gmail.com>
2023-11-12 00:52:39 +0500

    add

--- src/app/angular-bootstrap/state-crud/state-crud.component.ts
+++ src/app/angular-bootstrap/state-crud/state-crud.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-state-crud',
@@ -6,5 +7,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./state-crud.component.css']
 })
 export class StateCrudComponent {
+  StateValues: any = {}
+  UserFrom: FormBuilder | any
+
+
+constructor(private FormBuilder:FormBuilder) { 
+  this.userFromModel()
+}
+
+ngOnInit(): void {
+
+}
+
+userFromModel(){
+  this.UserFrom = this.FormBuilder.group({
+    name: new FormControl ('',[Validators.required]),
+    email: new FormControl ('',[Validators.required]),
+    first_name: new FormControl ('',[Validators.required,]),
+    last_name: new FormControl ('',[Validators.required,]),
+    address: new FormControl ('',[Validators.required,]),
+    city: new FormControl ('',[Validators.required,]),
+    country: new FormControl ('',[Validators.required,]),
+    pin_code: new FormControl ('',[Validators.required,]),
+    about_me: new FormControl ('',[Validators.required,]),
+    select1: new FormControl ('',[Validators.required,]), 
+    select2: new FormControl ('',[Validators.required,])  
+  })
+}
+
+
+Submit(){
+  const formValues = this.UserFrom.value
+  this.StateValues = formValues
+  console.log('formvalues',this.StateValues)
+}
+
+
 
 }
