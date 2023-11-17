a1c25da7a3df5acefc3bcc3e8a8c4e5b1283c2fd
amennajam009 <amennajam7@gmail.com>
2023-11-17 23:37:07 +0500

    add

--- src/app/angular-material/add-user-form/add-user-form.component.ts
+++ src/app/angular-material/add-user-form/add-user-form.component.ts
@@ -1,3 +1,4 @@
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
 import { Component } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 
@@ -7,10 +8,40 @@ import { MatDialog } from '@angular/material/dialog';
   styleUrls: ['./add-user-form.component.css']
 })
 export class AddUserFormComponent {
-  constructor(private _dialogue:MatDialog) { 
+
+  AngularMaterialForm: FormBuilder | any
+
+
+
+
+
+  constructor(private _dialogue:MatDialog,
+              private FormBuilder:FormBuilder) { 
+                this.UserFormModel()
   }
   
   ngOnInit(): void {
+  }
+
+
+  UserFormModel(){
+  this.AngularMaterialForm = this.FormBuilder.group({
+    first_name: new FormControl ('',[Validators.required]),
+    last_name: new FormControl ('',[Validators.required]),
+    email_address: new FormControl ('',[Validators.required,]),
+    education: new FormControl ('',[Validators.required,]),
+    house_no: new FormControl ('',[Validators.required,]),
+    last_name_2: new FormControl ('',[Validators.required,]),
+    checkbox_1: new FormControl ('',[Validators.required,]),
+    checkbox_2: new FormControl ('',[Validators.required,]),
+    checkbox_3: new FormControl ('',[Validators.required,]),
+    address: new FormControl ('',[Validators.required,]), 
+    city: new FormControl ('',[Validators.required,]),
+    country: new FormControl ('',[Validators.required,]),
+    pin_code: new FormControl ('',[Validators.required,]),
+    last_name_3: new FormControl ('',[Validators.required,]), 
+    about_me: new FormControl ('',[Validators.required,])  
+  })
   }
 
   close(){
