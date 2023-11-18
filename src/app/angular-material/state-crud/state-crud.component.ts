46f2a73fa8d3b3c8f96228678e1c37d083856f72
amennajam009 <amennajam7@gmail.com>
2023-11-18 14:59:15 +0500

    add

--- src/app/angular-material/state-crud/state-crud.component.ts
+++ src/app/angular-material/state-crud/state-crud.component.ts
@@ -3,11 +3,7 @@ import { AngularBootstrapCommunationService } from './../../angular-bootstrap/sh
 import { Component } from '@angular/core';
 import {MatDialog} from '@angular/material/dialog';
 import { AddUserFormComponent } from '../add-user-form/add-user-form.component';
-export interface UserData {
-  id: number;
-  name: string;
-  progress: string;
-}
+
 @Component({
   selector: 'app-state-crud',
   templateUrl: './state-crud.component.html',
@@ -21,15 +17,11 @@ export class StateCrudComponent {
 
 ngOnInit(): void {
 
-  // console.log('dataaaaaaaaaaaaa',this.AngularMaterialCommunicationService.getMyStateData())
 }
 
-displayedColumns: string[] = ['id', 'name', 'progress'];
-dataSource: UserData[] = [
-  { id: 1, name: 'John', progress: '20%' },
-  { id: 2, name: 'Doe', progress: '40%' },
-  { id: 3, name: 'Smith', progress: '60%' },
-];
+MyUserList(){
+  this.AngularMaterialCommunicationService.getMyStateData()
+}
 
 
 
