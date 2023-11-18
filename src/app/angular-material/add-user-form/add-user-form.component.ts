80d615540318829b2649e05f0bf58592c2898990
amennajam009 <amennajam7@gmail.com>
2023-11-18 13:02:16 +0500

    add

--- src/app/angular-material/add-user-form/add-user-form.component.ts
+++ src/app/angular-material/add-user-form/add-user-form.component.ts
@@ -44,14 +44,32 @@ export class AddUserFormComponent {
   })
   }
 
-  // MyPayLoad(){
-  //   const formValues = this.AngularMaterialForm.value;
-  //   console.log('myFormValeus',formValues)
-  // }
-
-  Submit(){
+  MyPayLoad(){
     const formValues = this.AngularMaterialForm.value;
     console.log('myFormValeus',formValues)
+    const payload:any = {
+      first_name: formValues.first_name,
+      last_name: formValues.last_name,
+      email_address: formValues.email_address,
+      education: formValues.education,
+      house_no: formValues.house_no,
+      last_name_2: formValues.last_name_2,
+      checkbox_1: formValues.checkbox_1,
+      checkbox_2: formValues.checkbox_2,
+      checkbox_3: formValues.checkbox_3,
+      address: formValues.address,
+      city: formValues.city,
+      country: formValues.country,
+      pin_code: formValues.pin_code,
+      last_name_3:formValues.last_name_3, 
+      about_me:formValues.about_me,
+    }
+    return payload
+  }
+
+  Submit(){
+    const formValues = this.MyPayLoad()
+    console.log('myFormValues',formValues)
   }
 
   close(){
