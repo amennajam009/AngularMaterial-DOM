bed63746b8c348e44e752b2a4a9f89142ecf8373
amennajam009 <amennajam7@gmail.com>
2023-12-05 16:03:37 +0500

    add

--- src/app/angular-bootstrap/types-of-directives/component-directive/component-directive.component.ts
+++ src/app/angular-bootstrap/types-of-directives/component-directive/component-directive.component.ts
@@ -2,7 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-component-directive',
-  templateUrl: './component-directive.component.html',
+  // templateUrl: './component-directive.component.html',
+  template: `<h1 class="text-center mt-5">Welcome to Component directive</h1>
+  <h2 class="text-center">You must be wondering i'm using html template file right? bruhh 💀</h2>
+  <h3>Go check Component directive.ts file</h3>`,
   styleUrls: ['./component-directive.component.css']
 })
 export class ComponentDirectiveComponent {
